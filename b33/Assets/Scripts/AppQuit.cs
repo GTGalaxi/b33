9bc5728460546a57741b84a149264d0592bdc0fa
Jack Floyd <jackadamfloyd@gmail.com>
2016-08-28 15:48:52 +1000

    Menus and shit

---
+++ b33/Assets/Scripts/AppQuit.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AppQuit : MonoBehaviour {
+
+	// Use this for initialization
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ b33/Assets/Scripts/levelselect.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class levelselect : MonoBehaviour {
+
+
+	public void changeToScene(int SceneToLoad)
+	{
+		SceneManager.LoadScene (SceneToLoad);
+	}
+
+}
