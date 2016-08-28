8c7e7595cd711149450106fd0e70c64473c0f6be
Jack Floyd <jackadamfloyd@gmail.com>
2016-08-28 13:23:14 +1000

    Nav mesh agent script, menus, lore, etc

---
+++ b33/Assets/Scripts/Playerdeath.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Playerdeath : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+
+		if(col.gameObject.tag == "bee")
+		{
+			
+			Application.LoadLevel ("Lose");
+
+		}
+
+	}
+
+}
---
+++ b33/Assets/Scripts/ResetVolume.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetVolume : MonoBehaviour 
+{
+	public string myObject;
+	public Vector3 resetPosition;
+
+	private GameObject mySphere;
+	private Rigidbody rigidBody;
+
+
+	void Start()
+	{
+		mySphere = GameObject.Find (myObject);
+		rigidBody = mySphere.GetComponent<Rigidbody> ();
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		// if ball exits reset volume;
+		if(other.gameObject == mySphere)
+		{
+			//reset position
+			other.transform.position = resetPosition;
+
+			// reset velocity
+			rigidBody.velocity = Vector3.zero;
+
+			//reset angular velocity
+			rigidBody.angularVelocity = Vector3.zero;
+
+			// reset orientation
+			other.transform.rotation = Quaternion.identity;
+
+		}
+	}
+}
+
