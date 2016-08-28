c8cb161a99847bfed9b03edc632b95d524f5ed18
cooper visser <coopervbusiness@gmail.com>
2016-08-28 15:44:37 +1000

    ASD

---
+++ b33/Assets/Lore.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Lore : MonoBehaviour {
+
+	public Image image;
+	// Use this for initialization
+	void Start () {
+
+		image.GetComponent<Image>();
+
+		image.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+		image.GetComponent<Image>();
+
+		if (Input.GetKeyDown (KeyCode.Escape) == true) 
+		{
+
+			image.enabled = false;
+
+		}
+
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+
+		if(col.gameObject.tag == "Player")
+		{
+
+			image.enabled = true;
+
+		}
+
+	}
+
+}
